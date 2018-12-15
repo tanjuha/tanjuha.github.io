9d02a722ef72a2c0f91b7250a1677fe80474a026
tanjuha <tanya.oleksandrivna19.07@gmail.com>
2018-12-15 17:03:54 +0300

    added filter adn some disign

---
+++ Hospital/Doctor_profile/js/filter.js
@@ -0,0 +1,16 @@
+function funFilter() {
+    var input, filter, ul, li, a;
+    input = document.getElementById('inputFilter');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("ulFilter");
+    li = ul.getElementsByTagName('li');
+
+    for(var i=0; i<li.length; i++){
+        a=li[i].getElementsByClassName("card-text ")[0];
+        if(a.innerHTML.toUpperCase().indexOf(filter) > -1){
+            li[i].style.display = "block";
+        }else{
+            li[i].style.display = "none";
+        }
+    }
+}
