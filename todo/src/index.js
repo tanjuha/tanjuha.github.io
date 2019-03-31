9ae6918bfd5f8c45eb69a5fd54e8433ba0ed0ee9
tanjuha <tanya.oleksandrivna19.07@gmail.com>
2019-04-01 02:08:17 +0300

    JSX

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,25 +2,47 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 const AppHeader = () => {
-   return  <h1>My Todo List</h1>
+    const styleHeader = {
+        color: 'red'
+    };
+
+
+   return  <h1 style={styleHeader}> My Todo List</h1>
 };
 
 const SearchPanel = () => {
-    return <input type="text" placeholder='Search'/>
+    const plaseholder = 'Search...';
+    return (
+
+        <input type="text"
+               className='bg-red'
+              /* htmlFor="name" */
+                title='search'
+               placeholder={plaseholder}
+        />
+
+    )
 };
 
 const TodoList = () => {
+    const items = ['Learn React', 'Build Awesome App']
     return (
         <ul>
-            <li>Learn React</li>
-            <li>Build Awesome App</li>
+            <li>{ items [0]}</li>
+            <li>{ items [1]}</li>
         </ul>
     )
 };
 
 const App = () => {
+    const log = false;
+    const login = <span>Login in plase <br/></span>
+    const logout = <span>Login out <br/></span>
+
     return (
         <div>
+            {log? login : logout }
+            <span>{ new Date().toString()}</span>
             <AppHeader />
             <SearchPanel />
             <TodoList />
