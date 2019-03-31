04ee781f07a43ef859c4108be8d4f46a39512592
tanjuha <tanya.oleksandrivna19.07@gmail.com>
2019-04-01 01:05:56 +0300

    React element

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const element = (
+    <div>
+        <h1>My Todo List</h1>
+        <input type="text" placeholder='Search'/>
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesome App</li>
+        </ul>
+    </div>
+);
+
+console.log(element);
+ReactDom.render(element, document.getElementById('root'));
