8e91aeac8b21165eb48f1576bec38136dab689b4
tanjuha <tanya.oleksandrivna19.07@gmail.com>
2019-04-19 02:43:28 +0300

    class - component

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -1,21 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './todo-list-item.css';
 
 
-// props - obj, exist every function like component first parameter
+export default class TodoListItem extends Component {
+    render (){
+        const { label, important = false } = this.props;
 
-/*const TodoListItem =(props) => {
-    return  <span>{ props.label }</span>
-};*/
+        const style = {
+            color: important ? 'tomato' : '#000'
+        };
 
-const TodoListItem =({ label, important = false }) => { /*destruction*/
-
-    const style = {
-        color: important ? 'tomato' : '#000'
-    };
-
-    return  (
-        <span className="todo-list-item row" >
+        return  (
+            <span className="todo-list-item row" >
             <span style={ style } className="todo-list-item col-lg-10">{ label } </span>
 
             <button type="button" className="btn btn-info btn-sm">
@@ -25,7 +21,12 @@ const TodoListItem =({ label, important = false }) => { /*destruction*/
                 <i className = "fa fa-trash-o"/>
             </button>
         </span>
-    );
+        );
+    };
 };
 
-export default TodoListItem;
+// props - obj, exist every function like component first parameter
+
+/*const TodoListItem =(props) => {
+    return  <span>{ props.label }</span>
+};*/
